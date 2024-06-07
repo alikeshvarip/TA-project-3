c4db00e75a71ae9576564e866eac2c9888df14aa
alikeshvrip <alikeshvari1383@gmail.com>
2024-06-07 20:32:01 +0330

    ایجاد کلاس

--- main.cpp
+++ main.cpp
@@ -1,29 +1,39 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-void reading_questions(int l, int s);
+class game
+{
+    public:
+    void reading_subject();
+    void one_player();
+    void reading_questions(int l, int s);
+}a;
 int main()
 {
+    a.reading_subject();
     int n;//تعداد بازیکنان
     while (true)
     {
         cin>>n;
         if (n==1)
-        one_player();
+        a.one_player();
         else if (n==2)
         ;
     }
     cout<<"seraj\n";
     return 0;
 }
-void one_player()
+void game:: reading_subject()
+{
+
+}
+void game:: one_player()
 {
     int l,s;// سطح سختی سوالات و موضوع سوالات
     cin>>l>>s;
     reading_questions(l, s);
 }
-void reading_questions(int l, int s)
+void game:: reading_questions(int l, int s)
 {
     if (l==1)
     ;
