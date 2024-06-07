94648084a2f1caa602797b61bd6329b372a746b2
alikeshvrip <alikeshvari1383@gmail.com>
2024-06-07 19:07:34 +0330

    تلاش هایی برای کسب نمره پروژه سوم

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;//تعداد بازیکنان
+    while (true)
+    {
+        cin>>n;
+        if (n==1)
+        ;
+        else if (n==2)
+        ;
+    }
+    cout<<"seraj\n";
+    return 0;
+}
+void one_player()
+{
+    int l;//سطح سختی سوالات
+    cin>>l;
+    if (l==1)
+    ;
+    else if (l==2)
+    ;
+    else if (l==3)
+    ;
+}
